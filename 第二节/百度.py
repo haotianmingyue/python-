9f0ec80f6320e2aa71e3be5d5715a433cff13f56
haotian <2421912570@qq.com>
2021-07-21 11:35:33 +0800

    爬百度，要把headers写的好一点

---
+++ 第二节/百度.py
@@ -0,0 +1,7 @@
+# 开发者 haotian
+# 开发时间: 2021/7/21 11:31
+import requests
+#所以只要把请求头 写的好一点 百度也是能爬的
+url = 'https://www.baidu.com/s?wd=python'
+r = requests.get(url,headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36'})
+print(r.request.url)
