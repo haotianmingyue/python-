2a06d0b39eeb1b6dcc7296d5ff79adf2ecce4d82
haotian <2421912570@qq.com>
2021-08-05 16:27:06 +0800

    它把一个选项去了，改了代码

--- 第二节/03.网络爬取大乐斗更新.py
+++ 第二节/03.网络爬取大乐斗更新.py
@@ -10,11 +10,11 @@ r = requests.get(url,headers={'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win6
 tree = etree.HTML(r)
 
 #更新的url后缀
-u_url = tree.xpath('//div[@class="i_newslist"]/ul[2]/li[1]/a[2]/@href')[0]
+u_url = tree.xpath('//div[@class="i_newslist"]/ul[1]/li[1]/a[2]/@href')[0]
 
 #新的url,得到更新内容的完整url
 n_url = url+u_url
-
+print(n_url)
 #所以爬取新的更新数据
 r_n = requests.get(n_url,headers={'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36'})
 
@@ -31,7 +31,7 @@ r_c = tree_n.xpath('//div[@class="Section1"]//text()')
 #更新时间
 time = tree_n.xpath('//div[@class="cnt"]/h3/text()')[0]
 
-# print(time)
+print(time)
 
 #转成字符串
 r_c = ''.join(r_c)
